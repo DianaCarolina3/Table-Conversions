1d2dbe02652ce9a08fd89cdf71f69dd18ea95cd9
DianaCarolina3 <dcaroli2015@gmail.com>
2021-03-25 12:50:43 -0500

    adding more convertions

--- app.js
+++ app.js
@@ -1,49 +1,60 @@
-let type = "onza"
-let amount = 3
+let type = "onzas"
+let amount = 5
 let ounce = ""
 let kilograms = ""
 let Pounds = ""
 let gram = ""
 
-// const toOunce = () => {
-//   if (type === "gramos") {
-//     ounce = amount / 28.35
-//     console.log('Onzas: ', ounce);
-//   } else if (type === "Onzas") {
-//     gram = amount * 28.35
-//     console.log('gram: ', gram)
-//   }
-// }
-// const toKilograms = () => {
-//   if (type === "gramos") {
-//     kilograms = amount / 1000
-//     console.log('kilogramos: ', kilograms);
-//   }
-// }
-// const toPounds = () => {
-//   if (type === "gramos") {
-//     pounds = amount / 454
-//     console.log('Libras: ', pounds)
-//   }
-// }
 
-const toResult = () => {
+const toResultToPounds = () => {
   switch(type) {
     case "gramo":
+      case "gramos":
       pounds = amount / 454
       console.log(`${amount} ${amount === 1 ? "gramo" : "gramos"} es igual a: ${pounds.toFixed(4)} Libras`)
-    break
+        break
     case "kilo":
-      pounds = amount * 2.205
-      console.log(`${amount} ${amount === 1 ? "kilo" : "kilos"} es igual a: ${pounds.toFixed(4)} Libras`)
-    break
+      case "kilos":
+        pounds = amount * 2.205
+        console.log(`${amount} ${amount === 1 ? "kilo" : "kilos"} es igual a: ${pounds.toFixed(4)} Libras`)
+          break
     case "onza":
-      pounds = amount / 16
-      console.log(`${amount} ${amount === 1 ? "onza" : "onzas"} es igual a: ${pounds.toFixed(4)} libras`)
-    break
-  }
-}
-toResult()
-// toOunce()
-// toKilograms()
-// toPounds()
+      case "onzas":
+        pounds = amount / 16
+        console.log(`${amount} ${amount === 1 ? "onza" : "onzas"} es igual a: ${pounds.toFixed(4)} libras`)
+          break
+    case "tonelada":
+      case "toneladas":
+        pounds = amount * 2205
+        console.log(`${amount} ${amount === 1 ? "tonelada" : "toneladas"} es igual a: ${pounds.toFixed(5)} libras`)
+          break
+        }
+      }
+      toResultToPounds()
+
+
+          // toOunce()
+          // toKilograms()
+          // toPounds()
+
+          // const toOunce = () => {
+          //   if (type === "gramos") {
+          //     ounce = amount / 28.35
+          //     console.log('Onzas: ', ounce);
+          //   } else if (type === "Onzas") {
+          //     gram = amount * 28.35
+          //     console.log('gram: ', gram)
+          //   }
+          // }
+          // const toKilograms = () => {
+          //   if (type === "gramos") {
+          //     kilograms = amount / 1000
+          //     console.log('kilogramos: ', kilograms);
+          //   }
+          // }
+          // const toPounds = () => {
+          //   if (type === "gramos") {
+          //     pounds = amount / 454
+          //     console.log('Libras: ', pounds)
+          //   }
+          // }
